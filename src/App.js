bbc828d2320dc218c9704aa07274812b04749794
squilari <lsquires898@gmail.com>
2018-11-18 18:12:39 -0800

    Fixed current option for search books and shelf

--- src/App.js
+++ src/App.js
@@ -70,57 +70,7 @@ class BooksApp extends React.Component {
     showSearchPage: false,
     searchedBooks: [],
     searchError: false,
-    books: [
-      {
-        cover:
-          "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
-        title: "To Kill a Mockingbird",
-        author: "Harper Lee",
-        option: "currentlyReading"
-      },
-      {
-        cover:
-          "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
-        title: "Ender's Game",
-        author: "Orson Scott Card",
-        option: "currentlyReading"
-      },
-      {
-        cover:
-          "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
-        title: "1776",
-        author: "David McCullough",
-        option: "wantToRead"
-      },
-      {
-        cover:
-          "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api",
-        title: "Harry Potter and the Sorcerer's Stone",
-        author: "J.K. Rowling",
-        option: "wantToRead"
-      },
-      {
-        cover:
-          "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api",
-        title: "The Hobbit",
-        author: "J.R.R. Tolkien",
-        option: "read"
-      },
-      {
-        cover:
-          "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api",
-        title: "Oh, the Places You'll Go!",
-        author: "Seuss",
-        option: "read"
-      },
-      {
-        cover:
-          "http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api",
-        title: "The Adventures of Tom Sawyer",
-        author: "Mark Twain",
-        option: "read"
-      }
-    ]
+    books: []
   };
 
   updateShelf = book => {
@@ -129,10 +79,15 @@ class BooksApp extends React.Component {
         b => b.title != book.title
       );
 
+      const updateSearch = currentState.searchBooks.map(
+        b => (b.title === book.title ? book : b)
+      );
+
       updatedBooks.push(book);
 
       return {
-        books: updatedBooks
+        books: updatedBooks,
+        searchBooks: updateSearch
       };
     });
   };
@@ -140,18 +95,21 @@ class BooksApp extends React.Component {
   updateQuery = query => {
     if (query) {
       BooksAPI.search(query, 25).then(data => {
-        this.setState(() => ({
+        this.setState(currentState => ({
           query: query,
           searchError: data.error ? true : false,
           searchBooks: data.error
             ? data.items
             : data.map(book => {
                 const tnail = book.imageLinks ? book.imageLinks.thumbnail : "";
+                const hasOption = currentState.books.filter(
+                  b => b.title === book.title
+                );
                 return {
                   cover: tnail,
                   author: book.authors,
                   title: book.title,
-                  option: "none"
+                  option: hasOption.length > 0 ? hasOption[0].option : "none"
                 };
               })
         }));
