27ff4ef3f417173c8d044dfde8d309a41577d953
squilari <lsquires898@gmail.com>
2018-11-25 14:55:23 -0800

    Moved call to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import React from "react";
+import * as BooksAPI from "./BooksAPI";
 import "./App.css";
 import { Search } from "./Search";
 import { Shelf } from "./Shelf";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { putOnShelf } from "./helpers";
 
 class BooksApp extends React.Component {
-  /**
-   * TODO: Instead of using this state variable to keep track of which page
-   * we're on, use the URL in the browser's address bar. This will ensure that
-   * users can use the browser's back and forward buttons to navigate between
-   * pages, as well as provide a good URL they can bookmark and share.
-   */
+  state = {
+    books: []
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(data =>
+      this.setState({
+        books: data
+      })
+    );
+  }
+
+  updateShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf).then(data =>
+      this.setState(currentState => {
+        return {
+          books: putOnShelf(currentState.books, data)
+        };
+      })
+    );
+  };
 
   render() {
+    const {
+      state: { books },
+      updateShelf
+    } = this;
+
     return (
       <div className="app">
         <Router>
           <div>
-            <Route exact path="/" component={Shelf} />
+            <Route
+              exact
+              path="/"
+              component={() => Shelf({ books, updateShelf })}
+            />
             <Route path="/search" component={Search} />
           </div>
         </Router>
--- src/Shelf.js
+++ src/Shelf.js
@@ -2,68 +2,38 @@ import React from "react";
 import * as BooksAPI from "./BooksAPI";
 import { Section } from "./Section";
 import { Link } from "react-router-dom";
-import { putOnShelf } from "./helpers";
 
-export class Shelf extends React.Component {
-  state = {
-    books: []
-  };
-
-  componentDidMount() {
-    BooksAPI.getAll().then(data =>
-      this.setState({
-        books: data
-      })
-    );
-  }
-
-  updateShelf = (book, shelf) => {
-    BooksAPI.update(book, shelf).then(data =>
-      this.setState(currentState => {
-        return {
-          books: putOnShelf(currentState.books, data)
-        };
-      })
-    );
-  };
-
-  render() {
-    const {
-      state: { books },
-      updateShelf
-    } = this;
-
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            <Section
-              books={books}
-              section="Currently Reading"
-              option="currentlyReading"
-              updateShelf={updateShelf}
-            />
-            <Section
-              books={books}
-              section="Want to Read"
-              option="wantToRead"
-              updateShelf={updateShelf}
-            />
-            <Section
-              books={books}
-              section="Read"
-              option="read"
-              updateShelf={updateShelf}
-            />
-          </div>
-        </div>
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
+export const Shelf = ({ books, updateShelf }) => {
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          <Section
+            books={books}
+            section="Currently Reading"
+            option="currentlyReading"
+            updateShelf={updateShelf}
+          />
+          <Section
+            books={books}
+            section="Want to Read"
+            option="wantToRead"
+            updateShelf={updateShelf}
+          />
+          <Section
+            books={books}
+            section="Read"
+            option="read"
+            updateShelf={updateShelf}
+          />
         </div>
       </div>
-    );
-  }
-}
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
+    </div>
+  );
+};
